39d71a43b8bc3d2f865e2a7388ef8e84ac92c4c8
Alexander Peplowski <alexander.peplowski@gmail.com>
2020-02-06 13:03:01 -0500

    rsync now targets data/* Updated paths in script files

--- run_evaluator.sh
+++ run_evaluator.sh
@@ -1,1 +1,1 @@
-python code/evaluator.py log/output.txt ../data/dummy_test_cfg.json -u="code/eval_user_cfg.json"
+python code/evaluator.py log/output.txt data/dummy_test_cfg.json -u="code/eval_user_cfg.json"
