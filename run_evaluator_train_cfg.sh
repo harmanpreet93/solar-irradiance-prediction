c1e5547e97e9530ae6115776f60abddb65a16261
Alexander Peplowski <alexander.peplowski@gmail.com>
2020-02-08 12:47:07 -0500

    Small fix to script

--- run_evaluator_train_cfg.sh
+++ run_evaluator_train_cfg.sh
@@ -1,1 +1,1 @@
-python code/evaluator.py output.txt train_cfg.json -u="code/eval_user_cfg.json" -s="output.txt"
+python code/evaluator.py log/output.txt train_cfg.json -u="code/eval_user_cfg.json" -s="output.txt"
