657e2df5069c5abac259acdadaaf9d8656611933
Alex <alexander.peplowski@gmail.com>
2020-02-12 20:27:39 +0000

    minor fixes

--- submit_batch_job.sh
+++ submit_batch_job.sh
@@ -3,15 +3,12 @@
 #SBATCH --gres=gpu:k80:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10000M
+#SBATCH --reservation=IFT6759_2020-02-12
 
-pwd
 module load python/3.7
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index -r requirements.txt
-# pip install --no-index tensorflow_gpu
 
-# python ./tensorflow-test.py
 ./run_training_loop.sh
 ./run_evaluator.sh
-sleep -10
