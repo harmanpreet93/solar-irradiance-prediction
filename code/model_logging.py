28e8354e90a33a08fdd6b412d06119ec1e32f8de
Alex Peplowski <alexander.peplowski@gmail.com>
2020-02-14 14:43:51 -0500

    Added tensorboard logging

--- code/model_logging.py
+++ code/model_logging.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+import tensorflow as tf
 
 logger = None
 
@@ -20,3 +21,12 @@ def get_logger():
         logger = logging.getLogger(__name__)
         logger.info("Logger initialized")
     return logger
+
+
+def get_summary_writer():
+    current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+    train_log_dir = 'log/gradient_tape/' + current_time + '/train'
+    test_log_dir = 'log/gradient_tape/' + current_time + '/test'
+    train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+    test_summary_writer = tf.summary.create_file_writer(test_log_dir)
+    return train_summary_writer, test_summary_writer
