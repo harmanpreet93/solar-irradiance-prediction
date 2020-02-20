24708469847f0a39aad6232ed2de69df6906f3ed
Alex Peplowski <alexander.peplowski@gmail.com>
2020-02-19 23:33:58 -0500

    Added json file generator based on daytime, valid ghi, image availability.

--- code/list_datetimes.py
+++ code/list_datetimes.py
@@ -12,31 +12,26 @@ def clean_df(df):
     df.replace(to_replace="nan", value=np.NaN, inplace=True)
 
 
-def get_datetimes_from_df(df, station_id):
-    daytime = df[station_id + "_DAYTIME"] == 1.0
-    valid_ghi = ~df[station_id + "_GHI"].isna()
-    valid_datetime = daytime & valid_ghi
+def get_datetimes_from_df(df):
+    '''returns datetimes with all valid GHI and at least one station daytime == 1.0'''
+    stations = ["BND", "TBL", "DRA", "FPK", "GWN", "PSU", "SXF"]
+
+    daytime = pd.DataFrame(index=df.index)
+    daytime['daytime'] = False
+    for station_id in stations:
+        station_daytime = df[station_id + "_DAYTIME"] == 1.0
+        daytime['daytime'] = station_daytime | daytime['daytime']
+
+    valid_ghi = pd.DataFrame(index=df.index)
+    valid_ghi['valid_ghi'] = True
+    for station_id in stations:
+        valid_ghi['valid_ghi'] = valid_ghi['valid_ghi'] & ~df[station_id + "_GHI"].isna()
+
+    valid_image = ~df["ncdf_path"].isna()
+
+    valid_datetime = daytime['daytime'] & valid_ghi['valid_ghi'] & valid_image
+
     return df.loc[valid_datetime].index
-
-
-# Check that all channels used are available
-def check_all_chan_ok(h5_data, hdf5_offset):
-    for channel in ["ch1", "ch2", "ch3", "ch4", "ch6"]:
-        dataset_lut_name = channel + "_LUT"
-        # This definition is used by the util package
-        if dataset_lut_name in h5_data and h5_data[dataset_lut_name][hdf5_offset] == -1:
-            return False
-    return True
-
-
-# Check if the file and offset exist on disk.
-def check_datetime_data_ok(hdf5_path, hdf5_offset):
-    if os.path.isfile(hdf5_path):
-        with h5py.File(hdf5_path, "r") as h5_data:
-            data_ok = check_all_chan_ok(h5_data, hdf5_offset)
-            if data_ok:
-                return True
-    return False
 
 
 def load_file(path, name):
@@ -55,40 +50,13 @@ def load_df(user_config):
     return pd.read_pickle(dataframe_path)
 
 
-def check_if_data_available(df, candidate_datetimes):
-
-    datetimes = []
-
-    for datetime in candidate_datetimes:
-        # For each retained datetime Lookup in the dataframe which file to check.
-        target_datetime = df.loc[datetime, ["hdf5_8bit_path", "hdf5_8bit_offset"]]
-
-        hdf5_path = target_datetime["hdf5_8bit_path"]
-        hdf5_offset = target_datetime["hdf5_8bit_offset"]
-
-        # # Temporary local implementation for df lookup; uses Jan 2015
-        # hdf5_path = "data/hdf5v7_8bit_Jan_2015/2015.01.01.0800.h5"
-
-        data_ok = check_datetime_data_ok(hdf5_path, hdf5_offset)
-        if data_ok:
-            datetimes += [datetime]
-
-    return datetimes
-
-
 def get_datetimes_with_past_im_avail(candidate_datetimes):
 
     nb_images_for_training = 3
     timedeltas = pd.timedelta_range(start='-15 min', periods=(nb_images_for_training-1), freq='-15 min')
-    print(candidate_datetimes[2])
-    print(candidate_datetimes[2] + timedeltas)
 
     final_datetimes = []
-    i = 0
     for candidate_datetime in candidate_datetimes:
-        i += 1
-        if i == 10:
-            break
         timedelta_ok = [elem in candidate_datetimes for elem in (candidate_datetime + timedeltas)]
         if all(timedelta_ok):
             final_datetimes += [candidate_datetime]
@@ -96,7 +64,7 @@ def get_datetimes_with_past_im_avail(candidate_datetimes):
     return final_datetimes
 
 
-def write_datetimes_to_json_cfg_file(train_timestamps, val_timestamps, user_config, station_id):
+def write_datetimes_to_json_cfg_file(train_timestamps, val_timestamps, user_config):
     train_datetimes = [ts.isoformat() for ts in train_timestamps]
     val_datetimes = [ts.isoformat() for ts in val_timestamps]
 
@@ -114,7 +82,7 @@ def write_datetimes_to_json_cfg_file(train_timestamps, val_timestamps, user_conf
         "dataframe_path": "data/catalog.helios.public.20100101-20160101.pkl",
         "start_bound": user_config["start_bound_train"],
         "end_bound": user_config["end_bound_train"],
-        "stations": {station_id: stations_dict[station_id]},
+        "stations": stations_dict,
         "target_time_offsets": [
             "P0DT0H0M0S",
             "P0DT1H0M0S",
@@ -124,7 +92,7 @@ def write_datetimes_to_json_cfg_file(train_timestamps, val_timestamps, user_conf
 
     train_output["target_datetimes"] = train_datetimes
 
-    with open('train_cfg_{}.json'.format(station_id), 'w') as outfile:
+    with open('train_cfg.json', 'w') as outfile:
         json.dump(train_output, outfile, indent=2)
 
     val_output = train_output
@@ -132,7 +100,7 @@ def write_datetimes_to_json_cfg_file(train_timestamps, val_timestamps, user_conf
     val_output["start_bound"] = user_config["start_bound_val"]
     val_output["end_bound"] = user_config["end_bound_val"]
 
-    with open('val_cfg_{}.json'.format(station_id), 'w') as outfile:
+    with open('val_cfg.json', 'w') as outfile:
         json.dump(val_output, outfile, indent=2)
 
 
@@ -156,17 +124,13 @@ def main(
     df = load_df(user_config)
     clean_df(df)
 
-    for station_id in ["BND", "TBL", "DRA", "FPK", "GWN", "PSU", "SXF"]:
+    candidate_datetimes = get_datetimes_from_df(df)
 
-        candidate_datetimes = get_datetimes_from_df(df, station_id)
+    processed_datetimes = get_datetimes_with_past_im_avail(candidate_datetimes)
 
-        datetime_availability_df = check_if_data_available(df, candidate_datetimes)
+    train_datetimes, val_datetimes = clip_datetimes(user_config, processed_datetimes)
 
-        processed_datetimes = get_datetimes_with_past_im_avail(datetime_availability_df)
-
-        train_datetimes, val_datetimes = clip_datetimes(user_config, processed_datetimes)
-
-        write_datetimes_to_json_cfg_file(train_datetimes, val_datetimes, user_config, station_id)
+    write_datetimes_to_json_cfg_file(train_datetimes, val_datetimes, user_config)
 
 
 def parse_args():
