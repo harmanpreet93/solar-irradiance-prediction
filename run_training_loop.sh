253bfb5782015008f6e26e9bc5bd3b6e8db43610
Alex Peplowski <alexander.peplowski@gmail.com>
2020-02-21 16:23:17 -0500

    Minor changes

--- run_training_loop.sh
+++ run_training_loop.sh
@@ -1,1 +1,1 @@
-python code/training_loop_launcher.py train_cfg.json val_cfg.json -u="code/eval_user_cfg.json"
+python code/training_loop_launcher.py train_cfg_local.json val_cfg_local.json -u="code/eval_user_cfg.json"
