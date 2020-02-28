e78c4ad77d6a512821caebde37bcae7f70becd4c
Harmanpreet Singh <singh.harmanpreet93@gmail.com>
2020-02-27 23:30:53 -0500

    remove old crops on new evaluation run

--- scripts/submit_evaluation.sh
+++ scripts/submit_evaluation.sh
@@ -3,8 +3,11 @@
 #SBATCH --gres=gpu:k20:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4000M
+
+rm -rf ../data/evaluator_script_val_cnn
 date
 source /project/cq-training-1/project1/teams/team08/server_env/bin/activate
+
 date
 echo ~~~~~~~~~~~evaluating now
 python ../code/evaluator.py ../log/output.txt ../val_cfg_local.json -u="../code/eval_user_cfg_cnn.json" -s="../log/output_stats.txt"
