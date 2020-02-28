a020a2f9560123dd701e3f09b62138f6e5d2d41e
Harmanpreet Singh <singh.harmanpreet93@gmail.com>
2020-02-27 23:28:45 -0500

    removed unnecessary files

--- scripts/rsync_data_harman.sh
+++
@@ -1,1 +0,0 @@
-rsync -avzh guest140@helios3.calculquebec.ca:/project/cq-training-1/project1/teams/team08/data/train_crops /Users/harman/Google\ Drive/advance_ML_project/data
--- scripts/tensorboard_sync.sh
+++ scripts/tensorboard_sync.sh
@@ -3,6 +3,6 @@ while :
 do
   echo "Removing logs"
   rm -r ../log/*
-  rsync -avzh guest140@helios3.calculquebec.ca:/home/guest140/harman_remote/log /Users/harman/workspace/sem2/advanced_project/Solar-irradiance-Team08-IFT6759
+  rsync -avzh guest140@helios3.calculquebec.ca:/home/guest140/team08/log .
   sleep 360
 done
